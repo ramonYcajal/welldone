c15c37a0ecef2f375fdba0df36d29694f6ee9a32
Juan Marcos Marin Perez <44023259+JuanMarcos10@users.noreply.github.com>
2019-10-20 00:44:57 +0200

    Test de automatic update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 const HomePage = () => {
     return(
         <h1 className="text-center">Welldone: Home Page</h1>
+        <h2 className="text-center">With Automatic update included</h2>
     )
 }
 
