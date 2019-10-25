b0b9db6ec38af0110a7559aabcc63cd61d02f498
David Rodriguez <david.rodriguez@innova-tsn.com>
2019-10-25 13:18:00 +0200

    Implementamos botón para volver atrás en la página de Erorr 404

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-const Page404 = () => {
+const Page404 = ({ history }) => {
+    console.log( history )
+
+    const goBack404 = e => {
+        e.preventDefault();
+        history.goBack();
+    }
+
     return(
-        <h1 className="text-center">Página no encontrada</h1>
+        <div className="text-center">
+            <h1>404: Página no encontrada</h1>
+            <form 
+                onSubmit={ goBack404 }
+            >
+                <button 
+                    type="submit" 
+                    className="btn btn-primary mb-2 mt-5">
+                        Sácame de Aquí!
+                </button>
+            </form>
+        </div>
     )
 }
 
-export default Page404;
+export default withRouter( Page404 );
