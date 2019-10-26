08a33ef9014d060987df361acaded2b3452e3c49
Antonio Santiago <tddmaspid@hotmail.com>
2019-10-26 22:12:38 +0200

    añadida etiqueta con imagen en archivo page404.js, creada carpeta images en carpeta src, añadida clase en archivo css para controlar la imagen

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,28 +1,30 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import error404 from '../images/error404.png'
 
 const Page404 = ({ history }) => {
-    console.log( history )
+    console.log(history)
 
     const goBack404 = e => {
         e.preventDefault();
         history.goBack();
     }
 
-    return(
+    return (
         <div className="text-center">
             <h1>404: Página no encontrada</h1>
-            <form 
-                onSubmit={ goBack404 }
+            <img className="imgError" src={error404}  alt=""></img>
+            <form
+                onSubmit={goBack404}
             >
-                <button 
-                    type="submit" 
+                <button
+                    type="submit"
                     className="btn btn-primary mb-2 mt-5">
-                        Sácame de Aquí!
+                    Sácame de Aquí!
                 </button>
             </form>
         </div>
     )
 }
 
-export default withRouter( Page404 );
+export default withRouter(Page404);
